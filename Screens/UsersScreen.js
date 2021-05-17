8b488d22cd08a6b9406746c2c9b6245d1429368a
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-05-17 16:18:43 +0200

    usersList in creation

--- Screens/UsersScreen.js
+++ Screens/UsersScreen.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Card, Badge, ListItem, Button, Icon } from 'react-native-elements';
+
+const users = [
+    {
+       name: 'Mathieu',
+       age: '20-35 ans',
+       avatar: require('../assets/mathieu_circle.png'),
+       availability: 'Soir & week-end',
+       hourMeeting: '17h à 19h'
+    },
+    {
+        name: 'Marilène',
+        age: '20-35 ans',
+        avatar: require('../assets/marilene_circle.png'),
+        availability: 'Soir & week-end',
+        hourMeeting: '17h à 19h'
+     },
+   ]
+   
 
 function UsersScreen() {
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: '#FF4757'
-        }}>
+
+        <ScrollView style={{ marginTop: 25 }}>
+
             <Text>
-                UsersScreen
+                Les sportifs autour de vous
             </Text>
-        </View>
+            
+            <Card>
+					<Card.Image
+						style={{ width: '100%', height: 170, marginBottom: 10 }}
+						source={users[0].avatar}
+					/>
+					<Badge status='success' value='Football' />
+
+				</Card>
+
+        </ScrollView>
     )
 }
 
