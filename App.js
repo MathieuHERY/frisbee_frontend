b5cc1c6ac369f7b1ccef6256cf17101909951d39
Axelle <crea@flipnpik.eu>
2021-05-18 13:50:32 +0200

    sign up

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']);
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs" ;
+
+// screens
+import ScreenInscription1 from "./screens/ScreenInscription1";
+import ScreenInscription2 from "./screens/ScreenInscription2";
+import ScreenInscription3 from "./screens/ScreenInscription3";
+import ScreenInscription4 from "./screens/ScreenInscription4";
+
+// navigation
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// redux
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="ScreenInscription1" component={ScreenInscription1} />
+          <Stack.Screen name="ScreenInscription2" component={ScreenInscription2} />
+          <Stack.Screen name="ScreenInscription3" component={ScreenInscription3} />
+          <Stack.Screen name="ScreenInscription4" component={ScreenInscription4} />
+        </Stack.Navigator>
+      </NavigationContainer>
+     
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
