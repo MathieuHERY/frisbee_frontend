eaf089d980e84e31fdc7f9573b74c5b95712d6b3
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-05-18 14:17:46 +0200

    get from Axelle

--- Screens/UsersScreen.js
+++ Screens/UsersScreen.js
@@ -1,30 +1,53 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
-import { Card, Badge, ListItem, Icon, Input } from 'react-native-elements';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Card, Badge, ListItem, Icon, Input, Button } from 'react-native-elements';
 import { FontAwesome } from '@expo/vector-icons';
 
 const users = [
     {
-        name: 'Mathieu',
-        age: '20-35 ans',
-        avatar: require('../assets/mathieu_circle.png'),
-        sport: 'football',
-        availability: 'Soir & week-end',
-        hourMeeting: '17h à 19h'
+        FirstName: 'Mathieu',
+        Age: '20-35 ans',
+        FavoritesSports: ['Football', 'Running'],
+        SportsHabits: 'Soir & week-end',
+        SportsHours: '17h à 19h',
+        UserPicture: require('../assets/mathieu_circle.png'),
+        UserLatitude: '37.78825',
+        UserLongitude: '-122.4324',
     },
     {
-        name: 'Marilène',
-        age: '20-35 ans',
-        avatar: require('../assets/marilene_circle.png'),
-        sport: 'Running',
-        availability: 'Soir & week-end',
-        hourMeeting: '17h à 19h'
+        FirstName: 'Marilène',
+        Age: '20-35 ans',
+        FavoritesSports: 'Running',
+        SportsHabits: 'Tous les jours',
+        SportsHours: '10h à 15h',
+        UserPicture: require('../assets/marilene_circle.png'),
+        UserLatitude: '37.78825',
+        UserLongitude: '-122.4324',
+    },
+    {
+        FirstName: 'Axelle',
+        Age: '20-35 ans',
+        FavoritesSports: 'Yoga',
+        SportsHabits: 'Weekend',
+        SportsHours: '9h à 20h',
+        UserPicture: require('../assets/axelle_circle.png'),
+        UserLatitude: '37.78825',
+        UserLongitude: '-122.4324',
     },
 ];
 
 
 function UsersScreen() {
-    const [text, setText] = useState('');
+
+    const [usersFiltered, setUsersFiltered] = useState([]);
+
+    useEffect( async () => {
+
+        var rawResponse = await fetch('http://172.16.190.9:3000/users');
+        var response = await rawResponse.json();
+        // setUsersFiltered();
+
+    }, []);
 
     return (
 
@@ -34,102 +57,83 @@ function UsersScreen() {
                 Les sportifs autour de vous
             </Text>
 
-            {/* La création d'une card en dur */}
-                <Card style={styles.container}>
-                    <View style={{ flex: 1}} >
-                        <Card.Image
-                            style={{ width: '35%', height: 100, marginBottom: 10 }}
-                            source={users[0].avatar}
-                        />
-                    </View>
-
-                    <View style={{ flex: 1}}>
-                        <Text>
-                            Mathieu
-                        </Text>
-
-                        <Text>
-                            20 - 35 ans
-                        </Text>
-
-                        <Badge status='success' value='Football' />
-                        <Text>
-                            <Icon
-                                type="fontawesome"
-                                name='calendar'
-                                size={24}
-                                color='#838383'
+            {
+                users.map((user, i) => (
+                    <Card containerStyle={styles.container}>
+                        {/* <View style={{ flex: 1 }} > */}
+                            <Card.Image
+                                style={{ width: '35%', height: 100, marginBottom: 10 }}
+                                source={users[i].UserPicture}
                             />
-                        Soir & week-end
-                        </Text>
 
-                        <Text>
-                            <Icon
-                                FontAwesome name='calendar'
-                                size={24}
-                                color='#838383'
-                            />
-                        17h à 19 h
-                        </Text>
+                        {/* <View style={{ flex: 1 }}> */}
+                        {/* <View> */}
+                            <Text>
+                                {user.FirstName}
+                            </Text>
 
-                        <Button
-                            title='Envoyer un FRISBEE'
-                            type= 'solid'
-                            style={{ color: '#00cec9' }}
-                            icon={
+                            <Text>
+                                {user.Age}
+                            </Text>
+
+                            <Badge status='success' value={user.FavoritesSports} />
+                            <Text>
                                 <Icon
-                                    name="user"
-                                    size={20}
-                                    color="#FFFFFF"
+                                    type="fontawesome"
+                                    name='calendar'
+                                    size={24}
+                                    color='#838383'
                                 />
-                            }
-                        />
-                    </View>
+                                {user.SportsHabits}
+                            </Text>
 
-                </Card>
+                            <Text>
+                                <Icon
+                                    FontAwesome name='calendar'
+                                    size={24}
+                                    color='#838383'
+                                />
+                                {user.SportsHours}
+                            </Text>
 
-
-            {/* CARD À DYNAMISER */}
-            <Card containerStyle={{ padding: 0 }} >
-                {
-                    users.map((user, i) =>
-                        <ListItem key={i} />)
-                }
-
-                <Card.Image
-                    style={{ width: '100%', height: 170, marginBottom: 10 }}
-                    source={users.avatar}
-                />
-
-                <Button
-                    title='Envoyer un FRISBEE'
-                    style={{ color: '#00CEC9' }}
-                    icon={
-                        <Icon
-                            name="user"
-                            size={20}
-                            color="#FFFFFF"
-                        />
-                    }
-                />
-
-            </Card>
+                            <Button
+                                title='Envoyer un FRISBEE'
+                                type='solid'
+                                style={{ color: '#00cec9' }}
+                                icon={
+                                    <Icon
+                                        name="user"
+                                        size={20}
+                                        color="#FFFFFF"
+                                    />
+                                }
+                                // onPress={() => {
+                                //     fetch('http://172.16.190.9.:3000/users', {
+                                //         method: 'POST',
+                                //         body: data 
+                                //     })
+                                // }
+                                // }
+                            />
+                        {/* </View> */}
+                    </Card>
+                ))
+            }
 
         </ScrollView>
     )
 }
 
 
-
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
-  });
+});
 
 
 export default UsersScreen;
